19109c43952d947290bc3edc789c316a8417958f
Natalia <smolutka15@gmail.com>
2021-03-31 20:59:45 +0200

    Add endpoints

--- server.js
+++ server.js
@@ -1,6 +1,67 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+  { id: 3, author: 'Julia Oe', text: 'They really know how to make you happy.' },
+  { id: 4, author: 'Paul Doera', text: 'They really know how to make you happy.' },
+  { id: 5, author: 'Grace Hosaoe', text: 'They really know how to make you happy.' },
+];
+
+const result = {message:'OK'}
+
+app.get('/testimonials', (req, res) => {
+  res.json(db);
+});
+
+app.get('/testimonials/random', (req, res) => {
+  res.json(db[Math.floor(Math.random() * db.length)]);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+  res.json(
+    db.filter(item => item.id === parseInt(req.params.id))
+  );
+});
+
+app.post('/testimonials', (req, res) => {
+  const {author, text} = req.body;
+  db.push({
+    id: uuidv4(),
+    author,
+    text,
+  });
+ res.json(result);
+});
+
+app.put('/testimonials/:id', (req, res) => {
+  const {author, text} = req.body;
+  const found = db.find(element => element.id === req.params.id);
+  const indexOfFoundElem = db.indexOf(found);
+  const updatedElem = ({
+    id: req.params.id,
+    author,
+    text
+  });
+  db[indexOfFoundElem] = updatedElem;
+  res.json(result);
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+  const found = db.find(element => element.id === req.params.id);
+  const indexOfFoundElem = db.indexOf(found);
+  db.splice(indexOfFoundElem, 1);
+  res.json(result);
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found...' });
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
